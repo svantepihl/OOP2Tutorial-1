f9e09b9abe67725994e71f90990c6676d8bbcf52
Svante Pihl <hej@svantepihl.com>
2020-10-02 15:40:48 +0200

    Tutorial completed

---
+++ OOP2Tutorial-1/ProductClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace OOP2Tutorial_1
+{
+    class Product
+    {
+        // Constructor that takes name as argument
+        public Product(string name)
+        {
+            Name = name;
+        }
+
+        // Auto-implemented readonly property:
+        private string Name;
+
+        // Getter for name
+        public string getName()
+        {
+            return Name;
+        }
+
+    }
+}
---
+++ OOP2Tutorial-1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OOP2Tutorial_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Product testProduct = new Product("Test");
+            Console.WriteLine(testProduct.getName());
+        }
+    }
+}
